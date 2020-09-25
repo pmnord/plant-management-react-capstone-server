134421eb4763974eaa04cef53ca62a05115bdcf1
Peter <peter.m.nordman@gmail.com>
2020-09-25 18:37:51 -0400

    Update URL for Trefle API

--- src/app.js
+++ src/app.js
@@ -6,49 +6,49 @@ const helmet = require('helmet');
 const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
-const authRouter = require('./auth/auth-router.js')
-const userRouter = require('./user/user-router')
-const gardenRouter = require('./garden/garden-router')
-const plantRouter = require('./plant/plant-router')
+const authRouter = require('./auth/auth-router.js');
+const userRouter = require('./user/user-router');
+const gardenRouter = require('./garden/garden-router');
+const plantRouter = require('./plant/plant-router');
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan(morganOption));
+app.use(helmet());
 app.use(
   cors({
-      origin: CLIENT_ORIGIN
+    origin: CLIENT_ORIGIN,
   })
-)
+);
 app.use((req, res, next) => {
-  const apiKey = req.get('api-key')
-  console.log(apiKey)
-  if (!apiKey) {return res.status(400).json({error: 'This server requires an API key'})}
-  if (apiKey != process.env.FANCYPLANTS_API_KEY) {return res.status(401).json({error: 'Invalid API key'})}
-  return next()
-})
+  const apiKey = req.get('api-key');
+  if (!apiKey) {
+    return res.status(400).json({ error: 'This server requires an API key' });
+  } else if (apiKey != process.env.FANCYPLANTS_API_KEY) {
+    return res.status(401).json({ error: 'Invalid API key' });
+  } else {
+    return next();
+  }
+});
 
 app.get('/', (req, res) => {
-  return res.status(200).end()
-})
+  return res.status(200).end();
+});
 
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
-app.use('/api/garden', gardenRouter)
-app.use('/api/plant', plantRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/garden', gardenRouter);
+app.use('/api/plant', plantRouter);
 
 app.use(function errorHandler(error, req, res, next) {
-      let response;
-      if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-      } else {
-        console.error(error)
-        response = { message: error.message, error }
-      }
-      res.status(500).json(response)
-    })
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
-
-module.exports = app
+module.exports = app;
--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: process.env.NODE_ENV === 'development'
-        ? `http://localhost:3000`
-        : process.env.CLIENT_ORIGIN,
-    TREFLE_API_BASE_URL: `https://trefle.io/api/`,
-    
-    JWT_SECRET: process.env.JWT_SECRET,
-    TREFLE_API_KEY: process.env.TREFLE_API_KEY,
-    FANCYPLANTS_API_KEY: process.env.FANCYPLANTS_API_KEY
-}
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN:
+    process.env.NODE_ENV === 'development'
+      ? `http://localhost:3000`
+      : process.env.CLIENT_ORIGIN,
+  TREFLE_API_BASE_URL: `https://trefle.io/api/`, // Deprecated
+
+  JWT_SECRET: process.env.JWT_SECRET,
+  TREFLE_API_KEY: process.env.TREFLE_API_KEY,
+  FANCYPLANTS_API_KEY: process.env.FANCYPLANTS_API_KEY,
+};
